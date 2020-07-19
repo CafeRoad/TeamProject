36647824ea42803eb062abe2f9d6085e11060d1b
sunjuho <sun_juho@naver.com>
2020-07-19 17:30:07 +0900

    이메일 인증 관련 테스트 중.

---
+++ src/main/java/com/sql/cafe/mail/MemberDTO.java
@@ -0,0 +1,38 @@
+package com.sql.cafe.mail;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDTO {
+
+	private String id;
+	private String password;
+	private String email;
+	private Date create_time;
+	private String tel;
+	private String authority;
+	
+	public String getId() {
+		return id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public Date getCreate_time() {
+		return create_time;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	
+	
+
+}
