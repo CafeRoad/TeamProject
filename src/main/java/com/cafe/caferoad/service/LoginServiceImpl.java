bbb996b416e84af9a1b343913ce0ae9934100038
sunjuho <sun_juho@naver.com>
2020-07-21 04:20:14 +0900

    무섭다

--- src/main/java/com/cafe/caferoad/service/LoginServiceImpl.java
+++ src/main/java/com/cafe/caferoad/service/LoginServiceImpl.java
@@ -5,25 +5,24 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.cafe.caferoad.dao.LoginDAO;
 import com.cafe.caferoad.vo.MemberVO;
 
-@Service
-public class LoginServiceImpl implements LoginService {
-	@Autowired
-	LoginDAO dao;
+//@Service
+//public class LoginServiceImpl implements LoginService {
+//	@Autowired
+//	LoginDAO dao;
+//
+//	@Override
+//	public String loginPro(MemberVO vo, HttpSession session) throws Exception {
+//		// TODO Auto-generated method stub
+//		String status = dao.loginPro(vo);
+//		if (status == null) {
+//			return null;
+//		} else {
+//			session.setAttribute("status", status);
+//			session.setAttribute("userid", vo.getUserid());
+//			return status;
+//		}
+//	}
 
-	@Override
-	public String loginPro(MemberVO vo, HttpSession session) throws Exception {
-		// TODO Auto-generated method stub
-		String status = dao.loginPro(vo);
-		if (status == null) {
-			return null;
-		} else {
-			session.setAttribute("status", status);
-			session.setAttribute("userid", vo.getUserid());
-			return status;
-		}
-	}
-
-}
+//}
